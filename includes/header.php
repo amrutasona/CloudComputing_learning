a13142597c5306cbcf358eb7acb0f4589e8512fb
Amruta <124129830+amrutasona@users.noreply.github.com>
2026-03-05 13:50:52 +0530

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>CloudClass</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<style>
+.navbar{
+    background: transparent;
+}
+.navbar-brand{
+    color:white !important;
+    font-weight:bold;
+}
+</style>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg position-absolute w-100">
+<div class="container">
+<a class="navbar-brand" href="index.php">CloudClass</a>
+</div>
+</nav>
